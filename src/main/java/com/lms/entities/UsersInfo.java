349dde911cfde413609d32511d6f8f84e1b5dc53
INTEK <arvindrajcs@gmail.com>
2024-10-15 23:29:58 +0530

    updated code: 15/10/24

--- src/main/java/com/lms/entities/UsersInfo.java
+++ src/main/java/com/lms/entities/UsersInfo.java
@@ -64,29 +64,36 @@ public class UsersInfo implements UserDetails {
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
     }
-
     @Override
     public String getPassword() {
-        return null;
+        return this.curPassword;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+    public String getName() {
+        return this.name;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
